6a9875deac752acd7bf567f80967b95ca46493c0
lipemiranda159 <lipemiranda159@hotmail.com>
2020-08-17 23:55:04 -0300

    add methods endpoint logger

--- src/main/java/challange/aceleradev/endpoints/LoggerController.java
+++ src/main/java/challange/aceleradev/endpoints/LoggerController.java
@@ -1,9 +1,30 @@
 package challange.aceleradev.endpoints;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import challange.aceleradev.entity.Log;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/logger")
 public class LoggerController {
+
+    @PostMapping("log")
+    public void AddLog(@RequestBody Log log)
+    {
+
+    }
+
+    @GetMapping("/all")
+    public List<Log> GetLosgs()
+    {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Log> findById(@PathVariable("id") Long id) {
+
+        return null;
+    }
 }

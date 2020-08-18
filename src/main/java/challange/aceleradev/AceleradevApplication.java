ea651cb623b94102cc683f3edb8ace61235e5ac7
lipemiranda159 <lipemiranda159@hotmail.com>
2020-08-17 23:15:37 -0300

    init api

---
+++ src/main/java/challange/aceleradev/AceleradevApplication.java
@@ -0,0 +1,18 @@
+package challange.aceleradev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AceleradevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AceleradevApplication.class, args);
+	}
+
+
+}
+
+

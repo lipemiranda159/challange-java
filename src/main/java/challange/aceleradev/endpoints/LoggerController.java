b4a22d1fc89d56ec72938ef9252e57fd2d9df8b8
lipemiranda159 <lipemiranda159@hotmail.com>
2020-08-17 23:20:07 -0300

    create logger controller

---
+++ src/main/java/challange/aceleradev/endpoints/LoggerController.java
@@ -0,0 +1,9 @@
+package challange.aceleradev.endpoints;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/logger")
+public class LoggerController {
+}
